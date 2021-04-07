f51bc5ade084f88d0d998a6bddd1be23a7ee5631
GOZ4EL <hordemzerado@gmail.com>
2021-04-07 00:00:05 -0400

    Terminando la validación del formulario mediante el uso de sesiones y un controlador de registro.

--- controllers/register.php
+++ controllers/register.php
@@ -1,36 +1,88 @@
 <?php
+session_start();
+$errors = false;
 $nameErr = $lastNameErr = $ciErr = $emailErr = $passwordErr = "";
 $name = $lastName = $ci = $email = $password = "";
 
 if($_SERVER["REQUEST_METHOD"] != "POST")
-    header("Location: ../..");
+    header("Location: ..");
 
-if(empty($name))
+#Validation for name and last name
+if(empty($_POST["name"])) {
     $nameErr = "Debe ingresar su nombre.";
-else
+    $errors = true;
+}else
     $name = testInput($_POST["name"]);
 
-if(empty($lastName))
+if(empty($_POST["lastname"])) {
     $lastNameErr = "Debe ingresar su apellido.";
-else 
+    $errors = true;
+}else 
     $lastName = testInput($_POST["lastname"]);
 
-if(empty($ci))
+if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
+    $nameErr = "Introduzca solamente letras y espacios.";
+    $errors = true;
+}
+if (!preg_match("/^[a-zA-Z-' ]*$/", $lastName)){
+    $lastNameErr = "Introduzca solamente letras y espacios.";
+    $errors = true;
+}
+
+#Validation for CI
+if(empty($_POST["CI"])) {
     $ciErr = "Debe ingresar su número de cedula.";
-else
+    $errors = true;
+}else
     $ci = testInput($_POST["CI"]);
 
-if(empty($email))
+if(!preg_match("/^[0-9]*$/", $ci) || strlen($ci) < 6 || strlen($ci) > 8) {
+    $ciErr = "Debe ingresar un número de cedula válido.";
+    $errors = true;
+}
+    
+#Validation for email
+if(empty($_POST["email"])) {
     $emailErr = "Debe ingresar su correo electrónico.";
-else
+    $errors = true;
+}else
     $email = testInput($_POST["email"]);
 
-if(empty($password))
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $emailErr = "El texto introducido no es una dirección valida de email.";
+    $errors = true;
+} 
+    
+#Validation for password
+if(empty($_POST["password"])) {
     $passwordErr = "Debe ingresar una contraseña.";
+    $errors = true;
+}
 else
     $password = testInput($_POST["password"]);
 
+if(strlen($password) < 6) {
+    $passwordErr = "La contraseña debe contener al menos 6 caracteres.";
+    $errors = true;
+}
     
+#Setting session variables for errors
+$_SESSION["nameErr"] = $nameErr;
+$_SESSION["lastNameErr"] = $lastNameErr;
+$_SESSION["ciErr"] = $ciErr;
+$_SESSION["emailErr"] = $emailErr;
+$_SESSION["passwordErr"] = $passwordErr;
+
+#Setting session variables for the input values
+$_SESSION["name"] = $name;
+$_SESSION["lastName"] = $lastName;
+$_SESSION["ci"] = $ci;
+$_SESSION["email"] = $email;
+
+$_SESSION["register"] = !$errors;
+
+header("Location: ..");
+
 function testInput(string $data) : string {
     $data = trim($data);
     $data = stripslashes($data);
