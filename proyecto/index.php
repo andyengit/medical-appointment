2ac20dd45e7d10777e99e040d2ecacaeab63302c
GOZ4EL <hordemzerado@gmail.com>
2021-04-17 23:57:44 -0400

    jkl

---
+++ proyecto/index.php
@@ -0,0 +1,94 @@
+<?php
+session_start();
+
+require_once "VARS.php";
+require_once "view/head.php";
+
+if(isset($_SESSION["register"]) && $_SESSION["register"]) {
+    $register = true;
+    session_destroy();
+    $_SESSION = null;
+}   
+?>
+
+<!--CONTENIDO-->
+<hr>
+<section class="modulos">
+    <h2>Registro</h2>
+
+	<form method="POST" action="controllers/register.php" id="register-form">
+        <label for="name">Nombre:</label>
+        <input 
+            class="data" type="text" id="name" name="name" 
+            value="<?= isset($_SESSION['name']) ? $_SESSION['name'] : '' ?>"
+        > 
+        <?php
+        if(isset($_SESSION["nameErr"])):?>
+            <strong class="alert_warning"><?= $_SESSION["nameErr"] ?></strong>
+        <?php 
+        endif;
+        ?>
+
+        <label for="lastname">Apellido:</label>
+        <input 
+            class="data" type="text" id="lastname" name="lastname" 
+            value="<?= isset($_SESSION['lastName']) ? $_SESSION['lastName'] : '' ?>"
+        >
+        <?php
+        if(isset($_SESSION["lastNameErr"])):?>
+            <strong class="alert_warning"><?= $_SESSION["lastNameErr"] ?></strong>
+        <?php 
+        endif;
+        ?>
+        
+        <label for="CI">Cedula de Identidad:</label>
+        <input 
+            class="data" type="text" id="CI" name="CI" 
+            value="<?= isset($_SESSION['ci']) ? $_SESSION['ci'] : '' ?>"
+        >
+        <?php
+        if(isset($_SESSION["ciErr"])):?>
+            <strong class="alert_warning"><?= $_SESSION["ciErr"] ?></strong>
+        <?php 
+        endif;
+        ?>
+
+
+        <label for="email">Correo Electrónico:</label>
+        <input 
+            class="data" type="email" id="email" name="email" 
+            value="<?= isset($_SESSION['email']) ? $_SESSION['email'] : '' ?>"
+        >
+        <?php
+        if(isset($_SESSION["emailErr"])):?>
+            <strong class="alert_warning"><?= $_SESSION["emailErr"] ?></strong>
+        <?php 
+        endif;
+        ?>
+
+        <label for="password">Contraseña:</label>
+        <input class="data" type="password" id="password" name="password">
+        <?php
+        if(isset($_SESSION["passwordErr"])):?>
+            <strong class="alert_warning"><?= $_SESSION["passwordErr"] ?></strong>
+        <?php 
+        endif;
+        ?>
+
+        <input class="botons" type="submit" value="Regístrate">
+
+        <p><a href="#">¿Ya tengo una cuenta?</a></p>
+
+        <?php
+        if(isset($register) && $register):?>
+            <strong class="alert_success">Registro completado correctamente.</strong>
+        <?php 
+            endif;
+        ?>
+    </form>
+
+    
+
+</section>
+
+<?php require_once "view/foot.php";
---
+++ proyecto/view/head.php
@@ -0,0 +1,46 @@
+<html lang="ES">
+	<head>
+		<meta charset="utf-8"/>
+		<title>REVEMED</title>
+		<style rel="icon" type="img/png"> 
+            <?php include_once($PATH."/view/img/estetos.png")?>
+        </style>
+		<style rel="stylesheet" type="text/css"> 
+            <?php include_once($PATH."/view/css/styles.css")?>
+        </style>
+		<style rel="stylesheet" type="text/css">
+            <?php include_once($PATH."/view/css/normalize.css")?>
+        </style>
+		<link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+		<script src="https://kit.fontawesome.com/28d6ce3668.js" crossorigin="anonymous"></script>
+	</head>
+    <body>
+        <header>
+            <main class="body">
+                <!--HEADER-->
+                <div class="header">
+                    <span><img src="http://localhost/proyecto/view/img/icon.png" alt="REVEMED"></span>
+                    <h1>REVEMED</h1>
+                </div>
+                <!--NOTICIAS-->
+                <section class="noticia">
+                    <div class="sombra"></div>
+                    <div class="contenedor">
+                        <img src="http://localhost/proyecto/view/img/noticia.jpg" />
+                        <div class="contenedor-text">
+                            <b>TU NUEVO PORTAL PARA AGENDAR CITAS MÉDICAS</b>
+                            <p>CON LOS DOCTORES Y CENTROS MÉDICOS DE TU PREFERENCIA</p>
+                        </div>
+                    </div>
+                </section>
+                <!--NAVEGACION-->
+                <section class="nav-menu">
+                    <div class="nav-sel"><a href="index.php"><span><img src="http://localhost/proyecto/view/img/casa.png"></span><b>INICIO</b></a></div>
+                    <div class="nav-sel"><a href="index.php?go=reservar"><span><img src="http://localhost/proyecto/view/img/Calendario.png"></span><b>RESERVAR CITA</b></a></div>
+                    <div class="nav-sel"><a href="index.php?go=examenes"><span><img src="http://localhost/proyecto/view/img/jeringa.png"></span><b>RESERVAR EXÁMENES</b></a></div>
+                    <div class="nav-sel"><a href="view/modulos/resultados.php"><span><img src="http://localhost/proyecto/view/img/libro.png" ></span><b>CONSULTAR CITA</b></a></div>
+                    <div class="nav-sel"><a href="#"><span><img src="http://localhost/proyecto/view/img/doc.png"></span><b>CONÓCENOS</b></a></div>
+                </section>
+            </main>
+        </header>
+        
