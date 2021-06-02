003d19a6b4eaeb3b3f22a01b2c21ff254ebe3821
GOZ4EL <hordemzerado@gmail.com>
2021-06-01 23:34:50 -0400

    Añadido de metodos para renderizar vistas

--- controllers/userController.php
+++ controllers/userController.php
@@ -4,5 +4,13 @@ class userController {
     public function index() {
         echo "Controlador User Accion Index.";
     }
+
+    public function login() {
+        require_once "../views/user/login.php";
+    }
+
+    public function register() {
+        require_once "../views/user/register.php";
+    }
 }
 
